edefe194b808a0ac8de6d03053311cb237fb6689
Anna Cunningham <ac232@hotmail.co.uk>
2021-01-24 17:59:37 +0000

    Added form submission for 2 players and more css elements

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -5,7 +5,7 @@ from app.models.game import Game
 
 @app.route('/')
 def index():
-    return render_template('base.html', title='RockPaperScissors')
+    return render_template('index.html', title='RockPaperScissors')
 
 @app.route('/play')
 def play_page():
@@ -19,18 +19,37 @@ def add_player():
     game = Game()
     choices = ["rock", "paper", "scissors"]
     computer_choice = game.make_choice(choices)
-    computer = Player("Computer", computer_choice)
+    computer = Player("The Computer", computer_choice)
     result = game.determine_winner(newplayer, computer)
     print(result)
     
     return render_template('result.html', title = 'RockPaperScissors', player1=newplayer, player2=computer, result=result)
   
+@app.route('/play2')
+def players_page():
+    return render_template('play2.html', title='RockPaperScissors')
+
+@app.route('/play2', methods=['POST'])
+def add_players():
+    name1 = request.form['name1']
+    choice1 = request.form['choice1']
+    player_1 = Player(name=name1, choice=choice1)
+    name2 = request.form['name2']
+    choice2 = request.form['choice2']
+    player_2 = Player(name=name2, choice=choice2)
+    game = Game()
+    result = game.determine_winner(player_1, player_2)
+    print(result)
+    
+    return render_template('result.html', title = 'RockPaperScissors', player1=player_1, player2=player_2, result=result)
+  
+
 
 @app.route('/play/result')
 def results():
     return render_template('result.html', title= 'RockPaperScissors')
 
-@app.route('/welcome')
+@app.route('/rules')
 def welcome_page():
     return render_template('rules.html', title = 'RockPaperScissors')
 
@@ -43,4 +62,4 @@ def play(player_1_choice, player_2_choice):
     print(result)
     
     
-    return render_template('index.html', title='RockPaperScissors', player1=player_1, player2=player_2, result=result)
+    return render_template('result.html', title='RockPaperScissors', player1=player_1, player2=player_2, result=result)
