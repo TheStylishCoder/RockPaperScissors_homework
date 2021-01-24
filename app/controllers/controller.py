11c49d60f3f0fa618371c4b467ca6093ebb9ec97
Anna Cunningham <ac232@hotmail.co.uk>
2021-01-24 13:56:27 +0000

    Added option to play computer

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect
+from flask import render_template, redirect, request
 from app import app
 from app.models.player import Player
 from app.models.game import Game
@@ -7,8 +7,28 @@ from app.models.game import Game
 def index():
     return render_template('base.html', title='RockPaperScissors')
 
-# @app.route('/', methods=['POST'])
-# def play():
+@app.route('/play')
+def play_page():
+    return render_template('play.html', title='RockPaperScissors')
+
+@app.route('/play', methods=['POST'])
+def add_player():
+    name = request.form['name']
+    choice = request.form['choice']
+    newplayer = Player(name=name, choice=choice)
+    game = Game()
+    choices = ["rock", "paper", "scissors"]
+    computer_choice = game.make_choice(choices)
+    computer = Player("Computer", computer_choice)
+    result = game.determine_winner(newplayer, computer)
+    print(result)
+    
+    return render_template('result.html', title = 'RockPaperScissors', player1=newplayer, player2=computer, result=result)
+  
+
+@app.route('/play/result')
+def results():
+    return render_template('result.html', title= 'RockPaperScissors')
 
 @app.route('/welcome')
 def welcome_page():
--- app/models/game.py
+++ app/models/game.py
@@ -1,18 +1,26 @@
+import random 
+
 class Game():
     
 
     def determine_winner(self, player_1, player_2):
-        if player_1.choice == "paper" and player_2.choice == "rock":
-            return "player 1 wins"
-        elif player_1.choice == "paper" and player_2.choice == "scissors":
-            return "player 2 wins"
-        elif player_1.choice == "rock" and player_2.choice == "paper":
-            return "player 2 wins"
-        elif player_1.choice == "rock" and player_2.choice == "scissors":
-            return "player 1 wins"
-        elif player_1.choice == "scissors" and player_2.choice == "paper":
-            return "player 1 won"
-        elif player_1.choice == "scissors" and player_2.choice == "rock":
-            return "player 2 has won"
+        if (player_1.choice.lower()) == "paper" and (player_2.choice.lower()) == "rock":
+            return "Player 1 wins"
+        elif (player_1.choice.lower()) == "paper" and (player_2.choice.lower()) == "scissors":
+            return "Player 2 wins"
+        elif (player_1.choice.lower()) == "rock" and (player_2.choice.lower()) == "paper":
+            return "Player 2 wins"
+        elif (player_1.choice.lower()) == "rock" and (player_2.choice.lower()) == "scissors":
+            return "Player 1 wins"
+        elif (player_1.choice.lower()) == "scissors" and (player_2.choice.lower()) == "paper":
+            return "Player 1 won"
+        elif (player_1.choice.lower()) == "scissors" and (player_2.choice.lower()) == "rock":
+            return "Player 2 has won"
         else:
             return None 
+
+
+    
+    def make_choice(self, choices):
+        return random.choice(choices)
+
